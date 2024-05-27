663dd4fa934f38c73c9a02bc27232a6edda8719a
Javed-Shekh777 <mdjavedshekh777.com>
2024-05-27 23:41:15 +0530

    response and error

--- src/index.js
+++ src/index.js
@@ -34,9 +34,19 @@
 // =========================================================================================
 
 import dotenv from 'dotenv';
+import app from './app.js';
+import { PORT } from './constants.js';
 import connectDB from "./db/db.js";
 dotenv.config({path:'./env'});
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed : ",err);
+});
 
 
 
